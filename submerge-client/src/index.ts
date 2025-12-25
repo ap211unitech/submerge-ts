bd02058f2d095e526e216dce65065febd210c795
Arjun Porwal <porwalarjun95@gmail.com>
2025-12-25 10:34:43 +0530

    chore: few more examples

--- submerge-client/src/index.ts
+++ submerge-client/src/index.ts
@@ -1,5 +1,26 @@
-import { BlockService, GenesisService } from "./client";
+import {
+  BlockService,
+  GenesisService,
+  EventService,
+  MetadataService,
+} from "./client";
 
-BlockService.getExtrinsicsByBlockReferenceAndIndex({
-  path: { block_ref: "234", extrinsic_index: 0 },
-}).then((res) => console.log(res.data));
+const main = async () => {
+  const [
+    { data: blocks },
+    { data: genesisRecords },
+    { data: events },
+    { data: metadataList },
+  ] = await Promise.all([
+    BlockService.getExtrinsicsByBlockReferenceAndIndex({
+      path: { block_ref: "234", extrinsic_index: 0 },
+    }),
+    GenesisService.getGenesisRecords(),
+    EventService.getEvents(),
+    MetadataService.getMetadataList(),
+  ]);
+
+  console.log(blocks, genesisRecords, events, metadataList);
+};
+
+main().catch(console.error);
